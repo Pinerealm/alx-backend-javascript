18a29ea0a213e816c692eb4dc5e1c6e96bec0a00
Pinerealm <osalami09@gmail.com>
2023-10-03 19:14:09 +0300

    Update the student grades using filter and map

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,10 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  const studentsByCity = students.filter((student) => student.location === city);
+  return studentsByCity.map((student) => {
+    const studentGrade = newGrades.filter((grade) => grade.studentId === student.id)[0];
+    if (studentGrade) {
+      return { ...student, grade: studentGrade.grade };
+    }
+    return { ...student, grade: 'N/A' };
+  });
+}
