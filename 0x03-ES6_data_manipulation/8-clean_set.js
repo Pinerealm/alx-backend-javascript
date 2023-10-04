8b52790695fa44ea69fea421d7971a7ba30820bb
Pinerealm <osalami09@gmail.com>
2023-10-04 06:15:22 +0300

    Return a string of set values having a substring

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,15 @@
+export default function cleanSet(set, startString) {
+  if (startString === '') {
+    return '';
+  }
+  if (typeof startString !== 'string') {
+    return '';
+  }
+  const result = [];
+  set.forEach((element) => {
+    if (element.startsWith(startString)) {
+      result.push(element.slice(startString.length));
+    }
+  });
+  return result.join('-');
+}
