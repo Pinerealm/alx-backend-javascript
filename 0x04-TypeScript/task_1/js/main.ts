742d5f8451edc90a420b266535afb0268ef5d090
Pinerealm <osalami09@gmail.com>
2023-10-07 07:40:13 +0300

    Define an interface, function and class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -7,3 +7,37 @@ interface Teacher {
     location: string;
     [key: string]: any;
 }
+
+// Define the director interface
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
+// Define the printTeacher function
+function printTeacher(firstName: string, lastName: string) {
+    return `${firstName.charAt(0)}. ${lastName}`;
+}
+
+// Define the printTeacherFunction interface
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+// Define the class StudentClass
+class StudentClass {
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework() {
+        return 'Currently working';
+    }
+
+    displayName() {
+        return this.firstName;
+    }
+}
