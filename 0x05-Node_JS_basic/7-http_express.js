abf892da1de96432ad518fcc49b8bc5831ba0cc0
Pinerealm <osalami09@gmail.com>
2023-11-25 02:45:13 +0300

    Wrap student list retrieval and response generation in an else block

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -15,32 +15,32 @@ app.get('/students', (req, res) => {
     if (err) {
       const errMessage = 'Cannot load the database';
       res.send(`This is the list of our students\n${errMessage}`);
-    }
+    } else {
+      let lines = data.split('\n');
+      lines = lines.filter((line) => line.length > 0);
+      lines.shift();
+      const fields = {};
 
-    let lines = data.split('\n');
-    lines = lines.filter((line) => line.length > 0);
-    lines.shift();
-    const fields = {};
+      lines.forEach((line) => {
+        const student = line.split(',');
+        if (!fields[student[3]]) {
+          fields[student[3]] = [];
+        }
+        fields[student[3]].push(student[0]);
+      });
 
-    lines.forEach((line) => {
-      const student = line.split(',');
-      if (!fields[student[3]]) {
-        fields[student[3]] = [];
+      const count = lines.length;
+      const resStringArray = ['This is the list of our students'];
+      resStringArray.push(`Number of students: ${count}`);
+      for (const field in fields) {
+        if (field) {
+          const list = fields[field];
+          const countStudents = list.length;
+          resStringArray.push(`Number of students in ${field}: ${countStudents}. List: ${list.join(', ')}`);
+        }
       }
-      fields[student[3]].push(student[0]);
-    });
-
-    const count = lines.length;
-    const resStringArray = ['This is the list of our students'];
-    resStringArray.push(`Number of students: ${count}`);
-    for (const field in fields) {
-      if (field) {
-        const list = fields[field];
-        const countStudents = list.length;
-        resStringArray.push(`Number of students in ${field}: ${countStudents}. List: ${list.join(', ')}`);
-      }
+      res.send(resStringArray.join('\n'));
     }
-    res.send(resStringArray.join('\n'));
   });
 });
 
