efa9354664c889bb906db564691e415e0d9004e9
Pinerealm <osalami09@gmail.com>
2023-11-25 06:07:11 +0300

    Implement testing using Chai assertion

---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,41 @@
+// Combining descriptions
+const expect = require('chai').expect;
+const calculateNumber = require('./1-calcul');
+
+describe('calculateNumber', () => {
+  describe('SUM', () => {
+    it('should return 4 when adding 1 and 3', () => {
+      expect(calculateNumber('SUM', 1, 3)).to.equal(4);
+    });
+    it('should return 5 when adding 1 and 3.7', () => {
+      expect(calculateNumber('SUM', 1, 3.7)).to.equal(5);
+    });
+    it('should return 5 when adding 1.2 and 3.7', () => {
+      expect(calculateNumber('SUM', 1.2, 3.7)).to.equal(5);
+    });
+  });
+
+  describe('SUBTRACT', () => {
+    it('should return -2 when subtracting 3 from 1', () => {
+      expect(calculateNumber('SUBTRACT', 1, 3)).to.equal(-2);
+    });
+    it('should return -3 when subtracting 3.7 from 1', () => {
+      expect(calculateNumber('SUBTRACT', 1, 3.7)).to.equal(-3);
+    });
+    it('should return 5 when subtracting 3.7 from 1.2', () => {
+      expect(calculateNumber('SUBTRACT', 1.2, 3.7)).to.equal(-3);
+    });
+  });
+
+  describe('DIVIDE', () => {
+    it('should return 5 when dividing 10 by 2', () => {
+      expect(calculateNumber('DIVIDE', 10, 2)).to.equal(5);
+    });
+    it('should return 2 when dividing 10 by 4.7', () => {
+      expect(calculateNumber('DIVIDE', 10, 4.7)).to.equal(2);
+    });
+    it('should return Error when dividing by 0', () => {
+      expect(calculateNumber('DIVIDE', 10, 0)).to.equal('Error');
+    });
+  });
+});
