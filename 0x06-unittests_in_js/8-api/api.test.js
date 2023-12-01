5ff1f20d0a6d9726bf5d7a95d6e924d19c30f2d3
Pinerealm <osalami09@gmail.com>
2023-12-01 17:24:59 +0300

    Add simple Express API server and integration test

---
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,20 @@
+// Integration test
+const request = require('request');
+const chai = require('chai');
+const { expect } = chai;
+
+describe('Index page', () => {
+  it('correct status code', (done) => {
+    request('http://localhost:7865', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('correct result', (done) => {
+    request('http://localhost:7865', (error, response, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
