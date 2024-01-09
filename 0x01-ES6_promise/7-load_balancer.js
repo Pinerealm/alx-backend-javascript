2074a5c53a75d0d962d7252deb0a492983932286
Pinerealm <osalami09@gmail.com>
2024-01-09 10:49:16 +0300

    Refactor loadBalancer function to remove unnecessary catch statements

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,6 +1,3 @@
 export default function loadBalancer(chinaDownload, USDownload) {
-  return Promise.race([
-    chinaDownload.catch(() => {}),
-    USDownload.catch(() => {}),
-  ]);
+  return Promise.race([chinaDownload, USDownload]);
 }
