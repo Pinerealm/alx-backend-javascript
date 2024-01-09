dc99baddb9140510012263a47f5b776321fb5649
Pinerealm <osalami09@gmail.com>
2024-01-09 11:06:09 +0300

    Refactor asyncUploadUser function to use Promise.all() for parallel execution

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -2,8 +2,7 @@ import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
   try {
-    const photo = await uploadPhoto();
-    const user = await createUser();
+    const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
     return { photo, user };
   } catch (err) {
     return { photo: null, user: null };
