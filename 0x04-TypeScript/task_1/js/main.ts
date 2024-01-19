4753307b2030817b807c4ff3cbdb33f2210660f5
Pinerealm <osalami09@gmail.com>
2024-01-19 09:25:33 +0300

    Refactor interfaces and classes in main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,54 +1,43 @@
 // Define the teacher interface
 interface Teacher {
-    readonly firstName: string;
-    readonly lastName: string;
-    fullTimeEmployee: boolean;
-    yearsOfExperience?: number;
-    location: string;
-    [key: string]: any;
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any;
 }
 
 // Define the director interface
 interface Directors extends Teacher {
-    numberOfReports: number;
+  numberOfReports: number;
 }
 
 // Define the printTeacher function
-function printTeacher(firstName: string, lastName: string) {
-    return `${firstName.charAt(0)}. ${lastName}`;
+function printTeacher(firstName: string, lastName: string): string {
+  return `${firstName.charAt(0)}. ${lastName}`;
 }
 
 // Define the printTeacherFunction interface
 interface printTeacherFunction {
-    (firstName: string, lastName: string): string;
+  (firstName: string, lastName: string): string;
 }
 
 // Define the class StudentClass
 class StudentClass {
-    firstName: string;
-    lastName: string;
+  firstName: string;
+  lastName: string;
 
-    constructor(firstName: string, lastName: string) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
 
-    workOnHomework() {
-        return 'Currently working';
-    }
+  workOnHomework(): string {
+    return 'Currently working';
+  }
 
-    displayName() {
-        return this.firstName;
-    }
-}
-
-// Define the StudentConstructor interface
-interface StudentConstructor {
-    new(firstName: string, lastName: string): StudentClass;
-}
-
-// Define the StudentClass interface
-interface StudentClassInterface extends StudentClass {
-    displayName(): string;
-    workOnHomework(): string;
+  displayName(): string {
+    return this.firstName;
+  }
 }
