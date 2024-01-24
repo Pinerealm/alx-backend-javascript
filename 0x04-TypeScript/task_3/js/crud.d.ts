7364f051dcfb84037f965c9eceea6b88f919c09a
Pinerealm <osalami09@gmail.com>
2024-01-24 22:02:24 +0300

    Export declarations in crud.d.ts

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -1,5 +1,5 @@
-import { RowID, RowElement } from "./interface";
+import { RowID, RowElement } from './interface';
 
-declare function insertRow(row: RowElement): RowID;
-declare function updateRow(rowId: RowID, row: RowElement): number;
-declare function deleteRow(rowId: RowID): void;
+export declare function insertRow(row: RowElement): RowID;
+export declare function updateRow(rowId: RowID, row: RowElement): RowID;
+export declare function deleteRow(rowId: RowID): void;
