ceb2a2cfc2a9711426ee6c022384c04386f5c102
Pinerealm <osalami09@gmail.com>
2024-01-26 16:29:43 +0300

    Refactor Java class and fix indentation

--- 0x04-TypeScript/task_4/js/subjects/Java.ts
+++ 0x04-TypeScript/task_4/js/subjects/Java.ts
@@ -1,18 +1,19 @@
+/// <reference path="./Subject.ts" />
 namespace Subjects {
-    export interface Teacher {
-        experienceTeachingJava?: number;
-    }
-    
-    export class Java extends Subject {
-        getRequirements(): string {
-        return "Here is the list of requirements for Java";
-        }
+  export interface Teacher {
+    experienceTeachingJava?: number;
+  }
 
-        getAvailableTeacher(): string {
-        if (this.teacher && this.teacher.experienceTeachingJava > 0) {
-            return `Available Teacher: ${this.teacher.firstName}`;
-        }
-        return "No available teacher";
-        }
+  export class Java extends Subject {
+    getRequirements(): string {
+      return 'Here is the list of requirements for Java';
     }
+
+    getAvailableTeacher(): string {
+      if (this.teacher && this.teacher.experienceTeachingJava > 0) {
+        return `Available Teacher: ${this.teacher.firstName}`;
+      }
+      return 'No available teacher';
+    }
+  }
 }
